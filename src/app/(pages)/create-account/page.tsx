7513325857b1b8ff8b05ba961c5936b56b2e2ce6
Kathy Zhang <112294375+b27b@users.noreply.github.com>
2024-06-17 01:58:19 -0400

    webpack issues

--- src/app/(pages)/create-account/page.tsx
+++ src/app/(pages)/create-account/page.tsx
@@ -8,6 +8,8 @@ import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import CreateAccountForm from './CreateAccountForm'
 
 import classes from './index.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default async function CreateAccount() {
   await getMeUser({
@@ -17,11 +19,32 @@ export default async function CreateAccount() {
   })
 
   return (
-    <Gutter className={classes.createAccount}>
-      <h1>Create Account</h1>
-      <RenderParams />
-      <CreateAccountForm />
-    </Gutter>
+    <section className={classes.createAccount}>
+    <div className={classes.heroImg}>
+      <Link href="/">
+        <Image
+          src="/logo-black.png"
+          alt="logo"
+          width={190}
+          height={23}
+          className={classes.logo}
+        />
+      </Link>
+    </div>
+
+    <div className={classes.formWrapper}>
+      <div className={classes.formContainer}>
+        <RenderParams className={classes.params}/>
+
+        <div className={classes.formTitle}>
+            <h3>Welcome</h3>
+            <p>Please enter details</p>
+
+            <CreateAccountForm/>
+        </div>
+      </div>
+    </div>
+  </section>
   )
 }
 

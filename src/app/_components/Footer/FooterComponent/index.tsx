f8bdf5ef303c5d255309ddeede0cc71abe1e0636
Kathy Zhang <112294375+b27b@users.noreply.github.com>
2024-06-16 23:47:55 -0400

    footer and some login page styling

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -1,9 +1,79 @@
+'use client' //remember to use this when usePathname !!!!
+
 import React from 'react'
 
-const FooterComponent = () => {
-    return (
-        <div>index</div>
-    )
+import classes from './index.module.scss'
+import { inclusions, noHeaderFooterUrls, profileNavItems } from '../../../constants'
+import { Gutter } from '../../Gutter'
+import { usePathname } from 'next/navigation'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Footer } from '../../../../payload/payload-types'
+import { Button } from '../../Button'
+
+const FooterComponent = ({footer} : {footer: Footer}) => {
+  const pathname = usePathname()
+  const navItems = footer?.navItems || [];
+
+  return (
+    <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
+      <Gutter>
+        <ul className={classes.inclusions}>
+          {inclusions.map((inclusion, index) => (
+            <li key={inclusion.title}>
+              <Image
+                src={inclusion.icon}
+                alt={inclusion.title}
+                width={32}
+                height={32}
+                className={classes.icon}
+              />
+
+              <h5 className={classes.title}>{inclusion.title}</h5>
+              <p>{inclusion.description}</p>
+            </li>
+          ))}
+        </ul>
+      </Gutter>
+
+      <div className={classes.footer}>
+        <Gutter>
+            <div className={classes.wrap}>
+                <Link href='/'>
+                <Image src='/logo-white.png' alt='logo' width={140} height={40}/>
+                </Link>
+                
+                <p>{footer.copyright}</p>
+
+                <div className={classes.socialLinks}>
+                  {navItems.map(item => {
+                    const icon = item?.link?.icon as Media;
+
+                    return(
+                      <Button
+                      key= {item.link.label}                        
+                      el="link"
+                      href={item.link.url}
+                      newTab = {true}
+                      className = {classes.socialLinkItem}
+                      >
+                       <Image
+                       src={icon?.url}
+                       alt={item.link.label}
+                       width={24}
+                       height={24}
+                       className={classes.socialIcon}
+                       />
+                      </Button>
+                    )
+                  })}
+                </div>
+            </div>
+        </Gutter>
+      </div>
+
+    </footer>
+  )
 }
 
-export default index 
+export default FooterComponent
--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -4,6 +4,9 @@ import Link from 'next/link'
 
 
 import classes from './index.module.scss'
+import FooterComponent from './FooterComponent'
+import { fetchFooter } from '../../_api/fetchGlobals'
+import { Footer } from '../../../payload/payload-types'
 
 export async function Footer() {
   let footer: Footer | null = null
